8909a928c9275a0a46e985d36fe265c68fe32767
Khaing01 <khaingphyuthin01@yahoo.com>
2017-08-16 11:39:32 +1000

    Initial commit

---
+++ app/src/main/java/com/example/khaingphyuthin/helloworld/Main2Activity.java
@@ -0,0 +1,15 @@
+package com.example.khaingphyuthin.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Log.d("MyApp","I am Activity 2");
+    }
+}
